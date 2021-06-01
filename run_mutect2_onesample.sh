765d914dccdb3fd6b897d89e7fc0246907beb1a5
martarodmo <56930829+martarodmo@users.noreply.github.com>
2021-06-01 22:43:30 +0200

    Version final de los archivos del TFM

--- run_mutect2_onesample.sh
+++ run_mutect2_onesample.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # sample specific arguments
 input="/home/proyectos/bioinfo/mrod/newResults_Oct20/results2"
 sample="48141131"
@@ -9,7 +8,6 @@ output="/home/proyectos/bioinfo/mrod/newResults_Oct20/arifacts_results3"
 # general arguments for our analysis
 bedfile="/home/proyectos/bioinfo/lodela/BioinfoUnit/martaLymp/3110861_Covered.bed"
 mutectscript="/home/proyectos/bioinfo/mrod/scriptsToMarta/sbatch_mutect2.sh"
-
 
 # run mutectscript with using these arguments: sample, input, output, bedfile
 
